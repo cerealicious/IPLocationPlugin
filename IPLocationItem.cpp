9d900078bfee47e9e85f9f39a32a8078a755e0e3
cerealicious <ceril_07@yahoo.com>
2026-06-21 15:38:58 -0400

    Refactor CIPLocationItem methods for clarity

--- IPLocationItem.cpp
+++ IPLocationItem.cpp
@@ -2,11 +2,23 @@
 #include "IPLocationPlugin.h"
 
 CIPLocationItem::CIPLocationItem() {}
+
 CIPLocationItem::~CIPLocationItem() {}
 
-const wchar_t* CIPLocationItem::GetItemName() const { return L"IP Location"; }
-const wchar_t* CIPLocationItem::GetItemId() const { return L"IPLocationPlugin"; }
-const wchar_t* CIPLocationItem::GetItemLableText() const { return L"IP"; }
+const wchar_t* CIPLocationItem::GetItemName() const
+{
+    return L"IP Location";
+}
+
+const wchar_t* CIPLocationItem::GetItemId() const
+{
+    return L"IPLocationPlugin";
+}
+
+const wchar_t* CIPLocationItem::GetItemLableText() const
+{
+    return L"IP";
+}
 
 const wchar_t* CIPLocationItem::GetItemValueText() const
 {
@@ -22,10 +34,24 @@ const wchar_t* CIPLocationItem::GetItemValueSampleText() const
     return L"255.255.255.255"; 
 }
 
-bool CIPLocationItem::IsCustomDraw() const { return false; }
-int CIPLocationItem::GetItemWidth() const { return 0; }
-void CIPLocationItem::DrawItem(void* hDC, int x, int y, int w, int h, bool dark_mode) {}
-int CIPLocationItem::OnMouseEvent(MouseEventType type, int x, int y, void* hWnd, int flag) { return 0; }
+bool CIPLocationItem::IsCustomDraw() const
+{
+    return false; // Let TrafficMonitor handle drawing
+}
+
+int CIPLocationItem::GetItemWidth() const
+{
+    return 0;
+}
+
+void CIPLocationItem::DrawItem(void* hDC, int x, int y, int w, int h, bool dark_mode)
+{
+}
+
+int CIPLocationItem::OnMouseEvent(MouseEventType type, int x, int y, void* hWnd, int flag)
+{
+    return 0;
+}
 
 void CIPLocationItem::SetIPInfo(const std::wstring& ip, const std::wstring& country)
 {
@@ -43,6 +69,7 @@ void CIPLocationItem::SetStatus(const std::wstring& status)
 {
     std::lock_guard<std::mutex> lock(m_mutex);
     m_status = status;
+    // Optionally update display text with status if IP is empty
     if (m_ip.empty())
         m_displayText = status;
 }
